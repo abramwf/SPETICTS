cdb3808f0935d16969f312508a5e7f78d8d456a6
FaridFatchur <m.farid.fatchur.rohmad-2021@fst.unair.ac.id>
2024-05-20 13:59:48 +0700

    database connect

--- speticts/settings.py
+++ speticts/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'speticts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spetics',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
 
