61b63fec79514d70faf0d6ac65c1a693df9fbd77
FaridFatchur <m.farid.fatchur.rohmad-2021@fst.unair.ac.id>
2024-06-07 13:50:11 +0700

    update model

--- analyticts/admin.py
+++ analyticts/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import User, Transcribe, Sentences
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Transcribe)
 admin.site.register(Sentences)
--- analyticts/models.py
+++ analyticts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(null=False)
-    profile_pic = models.TextField(null=False, default="https://pbs.twimg.com/media/GN_WWe5aUAAZk-S?format=jpg&name=4096x4096")
-    date_created = models.DateTimeField(auto_now_add=True)
+# class User(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     name = models.TextField(null=False)
+#     profile_pic = models.TextField(null=False, default="https://pbs.twimg.com/media/GN_WWe5aUAAZk-S?format=jpg&name=4096x4096")
+#     date_created = models.DateTimeField(auto_now_add=True)
 
 class Transcribe(models.Model):
     id = models.AutoField(primary_key=True)
@@ -13,6 +14,7 @@ class Transcribe(models.Model):
     title = models.TextField(null=False, default="none")
     trans_result = models.TextField(null=False)
     date_created = models.DateTimeField(auto_now_add=True)
+
 
 class Sentences(models.Model):
     class SATag(models.TextChoices) :
@@ -22,6 +24,8 @@ class Sentences(models.Model):
     id = models.AutoField(primary_key=True)
     id_trans = models.ForeignKey(Transcribe, on_delete=models.CASCADE, related_name='sentence', default=1)
     sentence = models.TextField(null=False)
-    sa_score = models.FloatField(null=False, default=0)
+    positive = models.DecimalField(max_digits=5, decimal_places=3, default=0.0)
+    negative = models.DecimalField(max_digits=5, decimal_places=3, default=0.0)
+    neutral = models.DecimalField(max_digits=5, decimal_places=3, default=0.0)
     sa_tag = models.CharField(max_length=1, choices=SATag.choices, default=SATag.NEUTRAL)
 
